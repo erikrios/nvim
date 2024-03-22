89a22dd248a1b760a9ff5b83de2f273805d514f4
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2024-03-22 13:51:01 +0700

    Show hidden files

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -7,6 +7,16 @@ return {
       "MunifTanjim/nui.nvim",
     },
     config = function()
-      vim.keymap.set('n', '<leader>e', ':Neotree filesystem reveal left<CR>', {})
+      require('neo-tree').setup {
+      filesystem = {
+        filtered_items = {
+          visible = true, 
+          hide_dotfiles = false,
+          hide_gitignored = true
+        }
+      }
+    }
+      
+    vim.keymap.set('n', '<leader>e', ':Neotree filesystem reveal left<CR>', {})
     end
   }
