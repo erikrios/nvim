69630712fb34b34093b81114d36172649d879d17
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2024-03-25 07:58:48 +0700

    Add autocomplete and snippets in neovim

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -25,17 +25,38 @@ return {
 	},
 	{
 		"neovim/nvim-lspconfig",
+    lazy = false,
 		config = function()
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
 			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({})
-			lspconfig.bashls.setup({})
-			lspconfig.dockerls.setup({})
-			lspconfig.docker_compose_language_service.setup({})
-			lspconfig.gopls.setup({})
-			lspconfig.jsonls.setup({})
-			lspconfig.markdown_oxide.setup({})
-			lspconfig.sqlls.setup({})
-			lspconfig.yamlls.setup({})
+			lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+      })
+			lspconfig.bashls.setup({
+        capabilities = capabilities,
+      })
+			lspconfig.dockerls.setup({
+        capabilities = capabilities,
+      })
+			lspconfig.docker_compose_language_service.setup({
+        capabilities = capabilities,
+      })
+			lspconfig.gopls.setup({
+        capabilities = capabilities,
+      })
+			lspconfig.jsonls.setup({
+        capabilities = capabilities,
+      })
+			lspconfig.markdown_oxide.setup({
+        capabilities = capabilities,
+      })
+			lspconfig.sqlls.setup({
+        capabilities = capabilities,
+      })
+			lspconfig.yamlls.setup({
+        capabilities = capabilities,
+      })
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
