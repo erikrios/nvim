9a79fb4d1520ce850b89b02f896cc7a2d3212fa7
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2025-06-05 10:13:24 +0700

    feat(file-path): add full path and file name

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,16 +1,24 @@
 return {
-  "nvim-lualine/lualine.nvim",
-  dependencies = {
-    "nvim-tree/nvim-web-devicons",
-  },
-  enabled = true,
-  lazy = false,
-  event = { "BufReadPost", "BufNewFile", "VeryLazy" },
-  config = function()
-    require("lualine").setup({
-      options = {
-        theme = "everforest"
-      }
-    })
-  end
+	"nvim-lualine/lualine.nvim",
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
+	enabled = true,
+	lazy = false,
+	event = { "BufReadPost", "BufNewFile", "VeryLazy" },
+	config = function()
+		require("lualine").setup({
+			options = {
+				theme = "everforest",
+			},
+			sections = {
+				lualine_c = {
+					{
+						"filename",
+						path = 1,
+					},
+				},
+			},
+		})
+	end,
 }
