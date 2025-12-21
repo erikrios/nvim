8e609ec7b8ac1ac3136d0bf58183919771213ff5
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2025-12-21 09:49:03 +0700

    chore: upgrade plugins version

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,7 +2,6 @@ return {
 	{
 		-- Find, Filter, Preview, Pick.
 		"nvim-telescope/telescope.nvim",
-		tag = "0.1.6",
 		dependencies = {
 			"nvim-lua/plenary.nvim",
 			"nvim-tree/nvim-web-devicons",
